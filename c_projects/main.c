22b1bcb40d1720b24fdd24c5cbdc6ab40daf1f90
Deyems <adeyemisola11@gmail.com>
2025-02-01 09:33:17 +0100

    usage of newline character and tab character double quotes

--- c_projects/main.c
+++ c_projects/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-int main() {
-    printf("!! Hello World !!");
+int main()
+{
+    printf("!! Hello World !!\n\n");
+    printf("the function has q quote \" usage ");
+    printf("when are we tab \t trying to cool");
     return 0;
 }
