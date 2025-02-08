a788dd3ec040cb5a5450be33c12d6de0b8be050b
Deyems <adeyemisola11@gmail.com>
2025-02-08 16:16:55 +0100

    using this file to document errors faced in c language coding

---
+++ c_projects/errors_in_c.c
@@ -0,0 +1,25 @@
+/**
+ * @file c_constants.c
+ * @author Adesola Adeyemi (https://github.com//Deyems/pg_works.git)
+ * @version 0.1
+ * @date 2025-02-08
+ * @copyright Copyright (c) 2025
+ * @brief
+ *
+ * c_constants.c:6:11: error: cannot assign to variable 'myNum' with const-qualified type 'const int'
+ * myNum = 57;
+ * ~~~~~ ^
+ * c_constants.c:5:15: note: variable 'myNum' declared const here
+ * const int myNum = 5;
+ * ~~~~~~~~~~^~~~~~~~~
+ * 1 error generated.
+*/
+
+/**
+ * @file c_constants.c
+ * @author Adesola Adeyemi (https://github.com//Deyems/pg_works.git)
+ * @version 0.1
+ * @date 2025-02-08
+ * @copyright Copyright (c) 2025
+ * @brief
+*/
