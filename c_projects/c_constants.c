b887b7390c76817f94274b5f7a88219924bf0370
Deyems <adeyemisola11@gmail.com>
2025-02-08 16:43:20 +0100

    standard practice of constant

--- c_projects/c_constants.c
+++ c_projects/c_constants.c
@@ -3,6 +3,8 @@
 int main(int argc, char const *argv[])
 {
     const int myNum = 57;
+    // It is good practice to declare constants as UPPERCASE.
+    const int BIRTH_YEAR = 2025;
     // myNum = 57;
     printf("Check if const can be changed %d \n", myNum);
     /* code */
