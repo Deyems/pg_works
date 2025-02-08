906899392af021c6b918b622b9d800ca4ad919cf
Deyems <adeyemisola11@gmail.com>
2025-02-08 16:16:33 +0100

    use of const to disallow modification of variables

---
+++ c_projects/c_constants.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    const int myNum = 57;
+    // myNum = 57;
+    printf("Check if const can be changed %d \n", myNum);
+    /* code */
+    return 0;
+}
