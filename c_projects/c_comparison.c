a5108282dc5330e46d2713cadc84d1f32b9d6350
Deyems <adeyemisola11@gmail.com>
2025-02-08 18:36:34 +0100

    comparison in c

---
+++ c_projects/c_comparison.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(int argc, char const *argv[])
+{
+    int x = 7;
+    int y = 9;
+
+    printf("compare x and y %d \t\n", y < x);
+
+    bool isProgrammingFun = true;
+    bool isItFun = false;
+
+    printf("checking how boolean shows up %d\t\n", isProgrammingFun);
+    printf("checking how boolean shows up %d\t\n", isItFun);
+    printf("%d", 10 == 11);
+    return 0;
+}
