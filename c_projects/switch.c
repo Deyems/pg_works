1849d8dd6e061620f5558862ad55678b76c7867b
Deyems <adeyemisola11@gmail.com>
2025-02-08 19:14:43 +0100

    using switch for condition routing

---
+++ c_projects/switch.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int day = 4;
+    switch (day)
+    {
+    case 1:
+        /* code */
+        printf("Monday");
+        break;
+
+    case 2:
+        /* code */
+        printf("Tuesday");
+        break;
+
+    case 3:
+        /* code */
+        printf("Wednesday");
+        break;
+    case 4:
+        /* code */
+        printf("Thursday");
+        break;
+
+    case 5:
+        /* code */
+        printf("Friday");
+        break;
+
+    case 6:
+        /* code */
+        printf("Saturday");
+        break;
+
+    case 7:
+        /* code */
+        printf("Sunday");
+        break;
+
+    default:
+        break;
+    }
+    /* code */
+    return 0;
+}
