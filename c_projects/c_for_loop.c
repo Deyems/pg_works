4534392d89be58412b6878a36a406bffa9aac8c0
Deyems <adeyemisola11@gmail.com>
2025-02-11 21:57:10 +0100

    using loops in c

---
+++ c_projects/c_for_loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    for(int idx = 0; idx < 5; idx++){
+        printf("Testing while loops are fun %d \n", idx);
+    }
+    /* code */
+    return 0;
+}
