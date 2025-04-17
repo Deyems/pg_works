568fe9097acec59d6bcd39012bf580bb3add1fca
Deyems <adeyemisola11@gmail.com>
2025-04-18 00:07:35 +0100

    get started with c++

---
+++ cpp_projects/c++_started.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // cout << "Hello World!" << endl;
+    cout << "Hello World! \n";
+    cout << "I am learning C++\n";
+    int store = 45;
+    int x, y, z;
+    x = y  = z = store;
+    cout << "The value stored is:" << store << " confirm it is!" << endl;
+    cout << x + y + z << "summation of assigned same values" << endl;
+    return 0;
+}
